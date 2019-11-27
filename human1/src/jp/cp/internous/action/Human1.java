6f4c693a429a7fdd5c2c74b91322a8fcb18a4204
hayashirikako <r1-kyatannnn6262@ezweb.ne.jp>
2019-11-27 20:35:40 +0900

    20191127

---
+++ human1/src/jp/cp/internous/action/Human1.java
@@ -0,0 +1,16 @@
+package jp.cp.internous.action;
+
+public class Human1 {
+   public static void main(String[]args){
+
+
+	   HumanName abc=new HumanName();
+	   System.out.println(abc.getName());
+
+	   HumanAge efg=new HumanAge();
+	   System.out.println(efg.getAge());
+
+	   HumanAddress hij=new HumanAddress();
+	   System.out.println(hij.getAddress());
+   }
+}
